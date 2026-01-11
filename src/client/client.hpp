b1f5caa9eb8a7e12fe81437d4ec30ba5a87d2b70
Torbjörn Rathsman <milasudril@gmail.com>
2026-01-11 17:32:29 +0100

    Add client API

---
+++ src/client/client.hpp
@@ -0,0 +1,35 @@
+#ifndef PROG_CLIENT_HPP
+#define PROG_CLIENT_HPP
+
+#include "src/io/io.hpp"
+
+/**
+ * \brief Contains the API published by the client
+ */
+namespace prog::client
+{
+	/**
+	 * \brief Represents the state of a client
+	 */
+	class state
+	{
+	public:
+		virtual ~state() = default;
+		/**
+		 * \brief This function should process data, by reading and writing associated file descriptors
+		 */
+		virtual void process_data() = 0;
+	};
+
+	/**
+	 * \brief Main entry point that creates a client
+	 * \param inputs File descriptors to read from
+	 * \param outputs File descriptors to write to
+   */
+	std::unique_ptr<client_state> create_client(
+		std::span<io::input_file_descriptor_ref const> inputs,
+		std::span<io::output_file_descriptor_ref const> outputs
+	);
+};
+
+#endif
