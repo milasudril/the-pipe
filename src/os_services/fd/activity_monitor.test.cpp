a1fb4c3cecaa189fe419a9abceb13c15c4be8245
Torbjörn Rathsman <milasudril@gmail.com>
2026-01-17 13:15:49 +0100

    Test epoll_fd_activity

--- src/os_services/fd/activity_monitor.test.cpp
+++ src/os_services/fd/activity_monitor.test.cpp
@@ -2,6 +2,7 @@
 
 #include "./activity_monitor.hpp"
 #include "src/os_services/fd/activity_event.hpp"
+#include "testfwk/validation.hpp"
 
 #include <testfwk/testfwk.hpp>
 
@@ -35,10 +36,64 @@ TESTCASE(prog_os_services_fd_epoll_event_to_activity_status)
 		prog::os_services::fd::epoll_event_to_activity_status(EPOLLOUT),
 		prog::os_services::fd::activity_status::write
 	);
-
 	EXPECT_EQ(
 		prog::os_services::fd::epoll_event_to_activity_status(EPOLLOUT|EPOLLIN),
 		prog::os_services::fd::activity_status::read_or_write
 	);
 }
 
+namespace
+{
+	struct my_epoll_entry_data_status
+	{
+		size_t dtor_callcount = 0;
+		prog::os_services::fd::activity_event const* last_activity_event{nullptr};
+	};
+
+	class my_epoll_entry_data:public prog::os_services::fd::epoll_entry_data
+	{
+	public:
+		explicit my_epoll_entry_data(my_epoll_entry_data_status& status):m_status{status}
+		{}
+
+		int get_fd_native_handle() const noexcept override
+		{ return 34; }
+
+		void handle_event(prog::os_services::fd::activity_event const& event) override
+		{
+			m_status.get().last_activity_event = &event;
+		}
+
+		~my_epoll_entry_data()
+		{ ++m_status.get().dtor_callcount;}
+
+		std::reference_wrapper<my_epoll_entry_data_status> m_status;
+	};
+}
+
+TESTCASE(prog_os_services_fd_epoll_fd_activity_no_valid_epoll_instance)
+{
+	my_epoll_entry_data_status status;
+	prog::os_services::fd::epoll_fd_activity activity{
+		new my_epoll_entry_data{status},
+		prog::os_services::fd::activity_status::read,
+		prog::os_services::fd::file_descriptor_ref{}
+	};
+
+	EXPECT_EQ(status.last_activity_event, nullptr);
+	activity.process();
+	EXPECT_EQ(status.last_activity_event, &activity);
+
+	try
+	{
+		activity.update_listening_status(prog::os_services::fd::activity_status::write);
+		abort();
+	} catch (...)
+	{ }
+
+	EXPECT_EQ(status.dtor_callcount, 0);
+	activity.close_fd();
+	EXPECT_EQ(status.dtor_callcount, 1);
+
+	EXPECT_EQ(activity.get_activity_status(), prog::os_services::fd::activity_status::read);
+}
