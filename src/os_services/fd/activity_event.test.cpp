c6c674620d470d074b3bcb82c17fe5f691fc9304
Torbjörn Rathsman <milasudril@gmail.com>
2026-01-17 12:45:16 +0100

    Add testcases for activity_event

---
+++ src/os_services/fd/activity_event.test.cpp
@@ -0,0 +1,21 @@
+//@	{"target":{"name": "activity_event.test"}}
+
+#include "./activity_event.hpp"
+
+#include <testfwk/testfwk.hpp>
+
+TESTCASE(prog_os_services_fd_activity_status_can_read)
+{
+	EXPECT_EQ(can_read(prog::os_services::fd::activity_status::none), false);
+	EXPECT_EQ(can_read(prog::os_services::fd::activity_status::read), true);
+	EXPECT_EQ(can_read(prog::os_services::fd::activity_status::write), false);
+	EXPECT_EQ(can_read(prog::os_services::fd::activity_status::read_or_write), true);
+}
+
+TESTCASE(prog_os_services_fd_activity_status_can_write)
+{
+	EXPECT_EQ(can_write(prog::os_services::fd::activity_status::none), false);
+	EXPECT_EQ(can_write(prog::os_services::fd::activity_status::read), false);
+	EXPECT_EQ(can_write(prog::os_services::fd::activity_status::write), true);
+	EXPECT_EQ(can_write(prog::os_services::fd::activity_status::read_or_write), true);
+}
