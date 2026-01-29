65be2873ec66e3f96d74931449313f295f05c3d4
Torbjörn Rathsman <milasudril@gmail.com>
2026-01-29 19:37:58 +0100

    Improve docs

---
+++ src/json_log/json_log.hpp
@@ -0,0 +1,11 @@
+#ifndef PIPE_JSON_LOG_HPP
+#define PIPE_JSON_LOG_HPP
+
+/**
+ * \brief Serialization and deserialisation of log messages to and from JSON
+ */
+namespace Pipe::json_log
+{
+}
+
+#endif
