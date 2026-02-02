003dbbcb15a5b13da0078377a0ae2d62b76c9554
Torbjörn Rathsman <milasudril@gmail.com>
2026-02-02 21:30:23 +0100

    Document client_info

--- src/client_ctl/client_info.hpp
+++ src/client_ctl/client_info.hpp
@@ -7,11 +7,22 @@
 
 namespace Pipe::client_ctl
 {
+	/**
+	 * \brief Contains information about a port
+	 */
 	struct port_info
 	{
+		/**
+		 * \brief The expected/promised content type of a port
+		 * \note There is no list of valid types, but a contract should be established within a
+		 * particular system
+		 */
 		std::string stream_content_type;
 	};
 
+	/**
+	 * \brief Converts a port_info ot a jopp::object
+	 */
 	inline jopp::object to_jopp_object(port_info const& obj)
 	{
 		jopp::object ret;
@@ -19,6 +30,9 @@ namespace Pipe::client_ctl
 		return ret;
 	}
 
+	/**
+	 * \brief Converts a jopp::object to a port_info
+	 */
 	inline port_info make_port_info(jopp::object const& obj)
 	{
 		return port_info{
@@ -26,8 +40,14 @@ namespace Pipe::client_ctl
 		};
 	}
 
+	/**
+	 * \brief A mapping from a port name to a port_info
+	 */
 	using port_info_map = std::map<std::string, port_info>;
 
+	/**
+	 * \brief Converts a port_info_map to a jopp::object
+	 */
 	inline jopp::object to_jopp_object(port_info_map const& obj)
 	{
 		jopp::object ret;
@@ -36,6 +56,9 @@ namespace Pipe::client_ctl
 		return ret;
 	}
 
+	/**
+	 * \brief Converts a jopp::object to a port_info_map
+	 */
 	inline port_info_map make_port_info_map(jopp::object const& obj)
 	{
 		port_info_map ret;
@@ -44,13 +67,30 @@ namespace Pipe::client_ctl
 		return ret;
 	}
 
+	/**
+	 * \brief Stores information about a client application
+	 */
 	struct client_info
 	{
+		/**
+		 * \brief A user-friendly name, independent of the name of the actual binary
+		 */
 		std::string display_name;
+
+		/**
+		 * \brief The available input ports
+		 */
 		port_info_map inputs;
+
+		/**
+		 * \brief The available output ports
+		 */
 		port_info_map outputs;
 	};
 
+	/**
+	 * \brief Converts a client_info to a jopp::object
+	 */
 	inline jopp::object to_jopp_object(client_info const& obj)
 	{
 		jopp::object ret;
@@ -60,6 +100,9 @@ namespace Pipe::client_ctl
 		return ret;
 	}
 
+	/**
+	 * \brief Converts a jopp::object to a client_info
+	 */
 	inline client_info make_client_info(jopp::object const& obj)
 	{
 		return client_info{
