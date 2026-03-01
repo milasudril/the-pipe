b8f450c2d451aa481c90f9d79b4134dd182d9447
Torbjörn Rathsman <milasudril@gmail.com>
2026-03-01 14:52:36 +0100

    Remove unused stuff

--- src/host/client_process.hpp
+++ src/host/client_process.hpp
@@ -12,22 +12,6 @@ namespace Pipe::host
 		struct client_ctl_tag{};
 		struct log_stream_tag{};
 
-		using ctl_response_event = os_services::fd::activity_event<
-			client_ctl_tag,
-			os_services::io::input_file_descriptor_tag
-		>;
-
-		using ctl_request_event = os_services::fd::activity_event<
-			client_ctl_tag,
-			os_services::io::output_file_descriptor_tag
-		>;
-
-		template<class T, class U>
-		void handle_event(os_services::fd::activity_event_handler_registered_event<T, U> const&)
-		{
-			// TODO: handle registration events
-		}
-
 		void handle_event(json_io::container_loaded_event<log_stream_tag>&& event);
 		void handle_event(json_io::parser_error_event<log_stream_tag> event);
 		void handle_event(json_io::input_closed_event<log_stream_tag>);
@@ -35,15 +19,6 @@ namespace Pipe::host
 		void handle_event(json_io::container_loaded_event<client_ctl_tag>&& event);
 		void handle_event(json_io::parser_error_event<client_ctl_tag> event);
 		void handle_event(json_io::input_closed_event<client_ctl_tag>);
-#if 0
-		void handle_event(ctl_response_event const&)
-		{
-		}
-
-		void handle_event(ctl_request_event const&)
-		{
-		}
-#endif
 
 		json_io::writer& get_ctl_output()
 		{ return m_ctl_output; }
