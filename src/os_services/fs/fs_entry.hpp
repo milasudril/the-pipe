513bbec296b3e2dcc565d4f64b1c0be95f3e243e
Torbjörn Rathsman <milasudril@gmail.com>
2026-03-11 18:54:23 +0100

    Add to_string for open_precondition

--- src/os_services/fs/fs_entry.hpp
+++ src/os_services/fs/fs_entry.hpp
@@ -2,10 +2,38 @@
 #define PIPE_OS_SERVICES_FS_FS_ENTRY_HPP
 
 #include "src/utils/utils.hpp"
+#include <stdexcept>
 
 namespace Pipe::os_services::fs
 {
 	enum class open_precondition{must_not_exist = -1, none = 0, must_exist = 1};
+
+	constexpr char const* to_string(open_precondition cond)
+	{
+		switch(cond)
+		{
+			case open_precondition::must_not_exist:
+				return "must_not_exist";
+			case open_precondition::none:
+				return "none";
+			case open_precondition::must_exist:
+				return "must_exist";
+			default:
+				throw std::range_error{""};
+		}
+	}
+
+	constexpr open_precondition make_open_precondition(std::string_view str)
+	{
+		if(str == "must_not_exist")
+		{ return open_precondition::must_not_exist; }
+		if(str == "none")
+		{ return open_precondition::none; }
+		if(str == "must_exist")
+		{ return open_precondition::must_exist; }
+
+		throw std::runtime_error{"Unknown opening precondition"};
+	}
 
 	enum class file_permission {
 		other_execute = 0001,
