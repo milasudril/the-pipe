44aae39afa22926cc6766824b479eec23483af4b
Torbjörn Rathsman <milasudril@gmail.com>
2026-03-11 21:31:34 +0100

    Convert file_access_permission to array of strings

--- src/os_services/fs/fs_entry.hpp
+++ src/os_services/fs/fs_entry.hpp
@@ -35,7 +35,7 @@ namespace Pipe::os_services::fs
 		throw std::runtime_error{"Unknown opening precondition"};
 	}
 
-	enum class file_access_permission_bit
+	enum class file_access_permission_bit:uint16_t
 	{
 		other_execute = 0,
 		other_write   = 1,
@@ -75,7 +75,7 @@ namespace Pipe::os_services::fs
 		}
 	}
 
-	constexpr file_access_permission_bit mkae_file_access_permission_bit(std::string_view str)
+	constexpr file_access_permission_bit make_file_access_permission_bit(std::string_view str)
 	{
 		if(str == "other_execute")
 		{ return file_access_permission_bit::other_execute; }
@@ -99,21 +99,38 @@ namespace Pipe::os_services::fs
 		throw std::runtime_error{"Unknown file permission bit"};
 	}
 
-
-	enum class file_access_permission
+	enum class file_access_permission:uint16_t
 	{
-		other_execute = 1<<static_cast<int>(file_access_permission_bit::other_execute),
-		other_write   = 1<<static_cast<int>(file_access_permission_bit::other_write  ),
-		other_read    = 1<<static_cast<int>(file_access_permission_bit::other_read   ),
-		group_execute = 1<<static_cast<int>(file_access_permission_bit::group_execute),
-		group_write   = 1<<static_cast<int>(file_access_permission_bit::group_write  ),
-		group_read    = 1<<static_cast<int>(file_access_permission_bit::group_read   ),
-		owner_execute = 1<<static_cast<int>(file_access_permission_bit::owner_execute),
-		owner_write   = 1<<static_cast<int>(file_access_permission_bit::owner_write  ),
-		owner_read    = 1<<static_cast<int>(file_access_permission_bit::owner_read   )
+		other_execute = 1 << static_cast<uint16_t>(file_access_permission_bit::other_execute),
+		other_write   = 1 << static_cast<uint16_t>(file_access_permission_bit::other_write  ),
+		other_read    = 1 << static_cast<uint16_t>(file_access_permission_bit::other_read   ),
+		group_execute = 1 << static_cast<uint16_t>(file_access_permission_bit::group_execute),
+		group_write   = 1 << static_cast<uint16_t>(file_access_permission_bit::group_write  ),
+		group_read    = 1 << static_cast<uint16_t>(file_access_permission_bit::group_read   ),
+		owner_execute = 1 << static_cast<uint16_t>(file_access_permission_bit::owner_execute),
+		owner_write   = 1 << static_cast<uint16_t>(file_access_permission_bit::owner_write  ),
+		owner_read    = 1 << static_cast<uint16_t>(file_access_permission_bit::owner_read   )
 	};
 
 	consteval void enable_bitmask_operators(file_access_permission){}
+
+	inline std::vector<char const*> to_array_of_strings(file_access_permission perm)
+	{
+		std::vector<char const*> ret;
+		ret.reserve(9);
+
+		uint16_t mask = 0400;
+		uint16_t k = 8;
+		while(mask != 0)
+		{
+			if(is_set(perm, file_access_permission{mask}))
+			{ ret.push_back(to_string(file_access_permission_bit{k})); }
+			--k;
+			mask >>= 1;
+		}
+
+		return ret;
+	}
 }
 
 #endif
