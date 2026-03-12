2127b8f1f9eec922d60f5ab3ecf429604c1a025b
Torbjörn Rathsman <milasudril@gmail.com>
2026-03-12 19:45:35 +0100

    Test to_array_of_strings

--- src/os_services/fs/file_access_permission.test.cpp
+++ src/os_services/fs/file_access_permission.test.cpp
@@ -1,6 +1,7 @@
 //@	{"target":{"name":"file_access_permission.test"}}
 
 #include "./file_access_permission.hpp"
+#include "testfwk/validation.hpp"
 
 #include <testfwk/testfwk.hpp>
 
@@ -107,5 +108,43 @@ TESTCASE(Pipe_os_services_fs_make_file_access_permission_bit)
 	catch(std::exception const& err)
 	{
 		EXPECT_EQ(err.what(), std::string_view{"Unknown file permission bit"});
+	}
+}
+
+TESTCASE(Pipe_os_services_fs_file_access_permission_value)
+{
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::other_execute), 0001);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::other_write), 0002);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::other_read), 0004);
+
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::group_execute), 0010);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::group_write), 0020);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::group_read), 0040);
+
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::owner_execute), 0100);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::owner_write), 0200);
+	EXPECT_EQ(static_cast<uint16_t>(Pipe::os_services::fs::file_access_permission::owner_read), 0400);
+}
+
+TESTCASE(Pipe_os_services_fs_file_access_permission_to_array_of_strings)
+{
+	{
+		auto const result = to_array_of_strings(
+			Pipe::os_services::fs::file_access_permission::owner_read|
+			Pipe::os_services::fs::file_access_permission::owner_write
+		);
+
+		REQUIRE_EQ(std::size(result), 2);
+		EXPECT_EQ(result[0], std::string_view{"owner_read"});
+		EXPECT_EQ(result[1], std::string_view{"owner_write"});
+	}
+
+	{
+		auto const result = to_array_of_strings(
+			Pipe::os_services::fs::file_access_permission::other_execute
+		);
+
+		REQUIRE_EQ(std::size(result), 1);
+		EXPECT_EQ(result[0], std::string_view{"other_execute"});
 	}
 }
