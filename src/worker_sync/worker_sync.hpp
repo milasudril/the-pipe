1ae0a17c482e701855adc1937226ea1a1e5e1416
Torbjörn Rathsman <milasudril@gmail.com>
2026-03-29 10:53:14 +0200

    Simplify worker_sync protocol

--- src/worker_sync/worker_sync.hpp
+++ src/worker_sync/worker_sync.hpp
@@ -1,80 +1,41 @@
 #ifndef PIPE_WORKER_SYNC_WORKER_SYNC_HPP
 #define PIPE_WORKER_SYNC_WORKER_SYNC_HPP
 
-#include <pthread.h>
 #include <string>
 #include <cstdint>
+#include <variant>
 
 namespace Pipe::worker_sync
 {
-	struct protocol_revision_request
-	{
-		std::vector<uint64_t> acceptable_versions;
-	};
+	struct port_activity_subscription_request
+	{ std::string server_portname; };
 
-	struct protocol_revision_response
-	{
-		uint64_t selected_version;
-	};
+	struct port_activity_unsubscription
+	{ uint64_t subscription_id; };
 
-	namespace v0
-	{
-		struct port_activity_subscription_request
-		{
-			std::string server_portname;
-		};
+	struct client_ready_event
+	{ uint64_t subscription_id; };
 
-		struct port_activity_subscription
-		{
-			uint64_t subscription_id;
-		};
+	using client_to_server_message = std::variant<
+		port_activity_subscription_request,
+		port_activity_unsubscription,
+		client_ready_event
+	>;
 
-		struct port_activity_unsubscription
-		{
-			uint64_t subscription_id;
-		};
+	struct port_activity_subscription
+	{ uint64_t subscription_id; };
 
-		struct port_activity_unsubscription_response
-		{};
+	struct port_activity_unsubscription_response
+	{};
 
-		struct data_ready_event
-		{
-			uint64_t subscription_id;
-		};
+	struct data_ready_event
+	{ uint64_t subscription_id; };
 
-		struct client_ready_event
-		{
-			uint64_t subscription_id;
-		};
-
-		enum class client_to_server_msg_types:uint32_t{
-			protocol_revision_request,
-			port_activity_subscription_request,
-			port_activity_unsubscription,
-			client_ready_event
-		};
-
-		struct request_header
-		{
-			uint64_t transaction_id;
-			client_to_server_msg_types msg_type;
-			uint32_t payload_size;
-		};
-
-		enum class server_to_client_msg_types:uint32_t{
-			protocol_revision_response,
-			port_activity_subscription,
-			port_activity_unsubscription_response,
-			data_ready_event
-		};
-
-		struct response_header
-		{
-			uint64_t transaction_id;
-			server_to_client_msg_types msg_type;
-			uint32_t payload_size;
-		};
-	}
+	using server_to_client_message = std::variant<
+		port_activity_subscription,
+		port_activity_unsubscription_response,
+		data_ready_event
+	>;
 }
 
 #endif
