638ab628d05bc270cf204880a037a1c87645ccdc
Torbjörn Rathsman <milasudril@gmail.com>
2026-04-21 22:08:59 +0200

    Add generic class sync_msg_codec, since similar logic will be used in both server and client

--- src/worker_fwk/sync_client_connection.hpp
+++ src/worker_fwk/sync_client_connection.hpp
@@ -3,6 +3,8 @@
 #ifndef PIPE_WORKER_FWK_SYNC_CLIENT_CONNECTION_HPP
 #define PIPE_WORKER_FWK_SYNC_CLIENT_CONNECTION_HPP
 
+#include "./sync_msg_codec.hpp"
+
 #include "./port_activity_subscription.hpp"
 #include "src/os_services/ipc/unix_domain_socket.hpp"
 #include "src/os_services/fd/activity_event_handler_store.hpp"
@@ -13,31 +15,28 @@
 
 namespace Pipe::worker_fwk
 {
-	class sync_client_connection
+	struct sync_msg_codec_server_traits
+	{
+		using fd_tag = os_services::ipc::connected_socket_tag<SOCK_STREAM, sockaddr_un>;
+		using incoming_sync_msg_type = worker_sync::client_to_server_message;
+		using outgoing_sync_msg_type = worker_sync::server_to_client_message;
+		struct client_activity{};
+		using sync_fd_activity_registred_event = 
+			os_services::fd::activity_event_handler_registered_event<client_activity, fd_tag>;
+		using sync_fd_activity_event = os_services::fd::activity_event<client_activity, fd_tag>;
+	};
+	
+	class sync_client_connection:
+		public sync_msg_codec<sync_msg_codec_server_traits>,
+		public sync_msg_codec_server_traits
 	{
 	public:
-		using fd_tag = os_services::ipc::connected_socket_tag<SOCK_STREAM, sockaddr_un>;
-
-		struct client_activity{};
-		using client_activity_event_handler_registered_event =
-			os_services::fd::activity_event_handler_registered_event<client_activity, fd_tag>;
-		using client_activity_event = os_services::fd::activity_event<client_activity, fd_tag>;
-
-		using msg_decoder = utils::wrap_variant_element_t<
-			utils::variant_push_front_t<worker_sync::client_to_server_message, worker_sync::msg_header>,
-			worker_sync::decoder
-		>;
-
-		using msg_encoder = utils::wrap_variant_element_t<
-			utils::variant_push_front_t<worker_sync::server_to_client_message, worker_sync::msg_header>,
-			worker_sync::encoder
-		>;
+		using client_activity_event_handler_registered_event = sync_fd_activity_registred_event;
+		using client_activity_event = sync_fd_activity_event;
 
 		explicit sync_client_connection(port_activity_subscriber_registry_ref port_activity_subscriber_registry, size_t buffer_size = 65536):
-			m_port_activity_subscriber_registry{port_activity_subscriber_registry},
-			m_buffer_size{buffer_size},
-			m_input_buffer{std::make_unique<std::byte[]>(buffer_size)},
-			m_output_buffer{std::make_unique<std::byte[]>(buffer_size)}
+			sync_msg_codec<sync_msg_codec_server_traits>{buffer_size},
+			m_port_activity_subscriber_registry{port_activity_subscriber_registry}
 		{}
 
 		~sync_client_connection();
@@ -46,57 +45,6 @@ namespace Pipe::worker_fwk
 		sync_client_connection& operator=(sync_client_connection&&) = default;
 		sync_client_connection(sync_client_connection const&) = delete;
 		sync_client_connection& operator=(sync_client_connection const&) = delete;
-
-		void handle_event(client_activity_event_handler_registered_event const& event)
-		{
-			auto const fd = event.fd.native_handle();
-			auto const flags = ::fcntl(fd, F_GETFL);
-			assert(flags != -1);
-			::fcntl(event.fd.native_handle(), F_SETFL, O_NONBLOCK|flags);
-			m_registration = event;
-		}
-
-		void handle_event(client_activity_event event)
-		{
-			assert(m_registration.event_handler_store != nullptr);
-
-			if(has_error(event.status)) [[unlikely]]
-			{
-				m_registration.event_handler_store->remove(m_registration.id);
-				return;
-			}
-
-			auto connection_status = connection_status::ok;
-			if(can_read(event.status))
-			{ connection_status = read_and_dispatch_requests(); }
-
-			if(can_write(event.status) && connection_status == connection_status::ok)
-			{ std::ignore = send_pending_messages(); }
-		}
-
-		enum class connection_status{ok, closed};
-
-		[[nodiscard]] connection_status read_and_dispatch_requests();
-
-		[[nodiscard]] connection_status send_pending_messages();
-
-		template<class T>
-		void send(T&& msg, worker_sync::transaction_id tx_id)
-		{
-			auto const msg_id = utils::variant_index_v<T, worker_sync::server_to_client_message>;
-			m_msgs_to_send.push(
-				worker_sync::encoder<worker_sync::msg_header>{
-					worker_sync::msg_header{
-						.msg_id = msg_id,
-						.tx_id = tx_id
-					}
-				}
-			);
-			m_msgs_to_send.push(
-				worker_sync::encoder<std::remove_cvref_t<T>>{std::forward<T>(msg)}
-			);
-			std::ignore = send_pending_messages();
-		}
 
 		void handle_request(
 			worker_sync::port_activity_subscription_request&& msg,
@@ -108,7 +56,6 @@ namespace Pipe::worker_fwk
 			worker_sync::transaction_id tx_id
 		)
 		{
-			m_currently_received_message = msg_decoder{};
 			auto const i = m_port_activity_subscriptions.find(msg.id);
 			if(i != std::end(m_port_activity_subscriptions))
 			{
@@ -122,15 +69,6 @@ namespace Pipe::worker_fwk
 
 		void handle_message(worker_sync::client_ready_event event)
 		{
-#if __GNUC__ == 13
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wfree-nonheap-object"
-#endif
-			m_currently_received_message = msg_decoder{};
-#if __GNUC__ == 13
-#pragma GCC diagnostic pop
-#endif
-
 			auto const i = m_port_activity_subscriptions.find(event.id);
 			if(i == std::end(m_port_activity_subscriptions))
 			{ throw std::runtime_error{"Subscription id not found"}; }
@@ -154,28 +92,10 @@ namespace Pipe::worker_fwk
 			);
 			i->second.client_status = client_status::ready;
 		}
+		
+		using sync_msg_codec<sync_msg_codec_server_traits>::handle_message;
 
 	private:
-		void handle_message(worker_sync::msg_header header)
-		{
-			utils::maybe_at_scope_exit on_exit{
-				[this](){
-					m_currently_received_message = msg_decoder{};
-				}
-			};
-
-			m_current_transaction_id = header.tx_id;
-			if(header.msg_id == std::numeric_limits<decltype(header.msg_id)>::max())
-			{ throw std::runtime_error{"Invalid type-id"}; }
-
-			m_currently_received_message = utils::make_variant<msg_decoder>(header.msg_id + 1);
-			on_exit.reset();
-		}
-
-		void enable_write_listening();
-
-		void disable_write_listening();
-
 		port_activity_subscriber_registry_ref m_port_activity_subscriber_registry;
 
 		enum class client_status{ready, busy};
@@ -189,21 +109,6 @@ namespace Pipe::worker_fwk
 		std::unordered_map<worker_sync::port_activity_subscription_id, output_port_info> m_port_activity_subscriptions;
 		worker_sync::port_activity_subscription_id m_port_activity_subscription_id{0};
 
-		size_t m_buffer_size;
-
-		// Decoder
-		std::unique_ptr<std::byte[]> m_input_buffer;
-		std::span<std::byte const> m_bytes_left_to_process;
-		msg_decoder m_currently_received_message;
-		worker_sync::transaction_id m_current_transaction_id;
-
-		// Encoder
-		std::unique_ptr<std::byte[]> m_output_buffer;
-		std::span<std::byte const> m_bytes_to_write;
-		std::queue<msg_encoder> m_msgs_to_send;
-		bool m_is_listening_for_write{false};
-
-		client_activity_event_handler_registered_event m_registration;
 	};
 }
 #endif
---
+++ src/worker_fwk/sync_msg_codec.hpp
@@ -0,0 +1,258 @@
+#ifndef PIPE_WORKER_FWK_SYNC_MSG_CODEC_HPP
+#define PIPE_WORKER_FWK_SYNC_MSG_CODEC_HPP
+
+#include "src/os_services/io/io.hpp"
+#include "src/os_services/fd/activity_event_handler_store.hpp"
+#include "src/worker_sync/worker_sync.hpp"
+#include "src/utils/utils.hpp"
+
+#include <fcntl.h>
+#include <memory>
+#include <cstddef>
+#include <span>
+#include <queue>
+
+namespace Pipe::worker_fwk
+{
+	template<class SyncMsgCodecTraits>
+	class sync_msg_codec
+	{
+	private:
+		using taits = SyncMsgCodecTraits;
+		using incoming_msg_type = typename taits::incoming_sync_msg_type;
+		using outgoing_msg_type = typename taits::outgoing_sync_msg_type;
+		using msg_decoder = utils::wrap_variant_element_t<
+			utils::variant_push_front_t<worker_sync::client_to_server_message, worker_sync::msg_header>,
+			worker_sync::decoder
+		>;
+		using msg_encoder = utils::wrap_variant_element_t<
+			utils::variant_push_front_t<worker_sync::server_to_client_message, worker_sync::msg_header>,
+			worker_sync::encoder
+		>;
+		using transaction_id = worker_sync::transaction_id;
+		using fd_activity_registred_event = typename taits::sync_fd_activity_registred_event;
+		using fd_activity_event = typename taits::sync_fd_activity_event;
+
+	public:
+		explicit sync_msg_codec(size_t buffer_size):
+			m_buffer_size{buffer_size},
+			m_input_buffer{std::make_unique<std::byte[]>(buffer_size)},
+			m_output_buffer{std::make_unique<std::byte[]>(buffer_size)}
+		{}
+
+		void handle_event(fd_activity_registred_event reg_event)
+		{
+			auto const fd = reg_event.fd.native_handle();
+			auto const flags = ::fcntl(fd, F_GETFL);
+			assert(flags != -1);
+			::fcntl(reg_event.fd.native_handle(), F_SETFL, O_NONBLOCK|flags);
+			m_registration = reg_event;
+
+		}
+
+		template<class Self>
+		void handle_event(this Self& self, fd_activity_event event)
+		{
+			assert(self.m_registration.event_handler_store != nullptr);
+
+			if(has_error(event.status)) [[unlikely]]
+			{
+				self.m_registration.event_handler_store->remove(self. m_registration.id);
+				return;
+			}
+
+			auto connection_status = connection_status::ok;
+			if(can_read(event.status))
+			{ connection_status = self.read_and_dispatch_requests(); }
+
+			if(can_write(event.status) && connection_status == connection_status::ok)
+			{ std::ignore = self.send_pending_messages(); }
+		}
+
+		enum class connection_status{ok, closed};
+
+		template<class Self>
+		[[nodiscard]] connection_status read_and_dispatch_requests(this Self& self)
+		{
+			auto const read_result = Pipe::os_services::io::read_full(
+				self.m_registration.fd, std::span{self.m_input_buffer.get(), self.m_buffer_size}
+			);
+			if(read_result.bytes_transferred() == 0)
+			{
+				if(!read_result.operation_would_have_blocked())
+				{
+					self.m_registration.event_handler_store->remove(self.m_registration.id);
+					return connection_status::closed;
+				}
+				return connection_status::ok;
+			}
+
+			auto bytes_to_process = std::span{self.m_input_buffer.get(), read_result.bytes_transferred()};
+			while(!bytes_to_process.empty())
+			{
+				auto const bytes_consumed = std::visit(
+					[&self, bytes_to_process]<class Decoder>(Decoder& item) {
+						return item.decode_and_dispatch(
+							bytes_to_process,
+							[&self]<class Msg>(Msg&& item){
+								utils::maybe_at_scope_exit reset_decoder{
+									[&self](){
+										self.m_currently_received_message = {};
+									}
+								};
+
+								if constexpr(std::is_same_v<std::remove_cvref_t<Msg>, worker_sync::msg_header>)
+								{ reset_decoder.reset(); }
+
+								if constexpr(
+									requires{
+										{self.handle_request(std::forward<Msg>(item), self.m_current_transaction_id)};
+									}
+								)
+								{ self.handle_request(std::forward<Msg>(item), self.m_current_transaction_id); }
+								else
+								{ self.handle_message(std::forward<Msg>(item)); }
+							},
+							[&self](worker_sync::error_response&& response) {
+								utils::at_scope_exit reset_decoder{
+									[&self](){
+										self.m_currently_received_message = {};
+									}
+								};
+								self.send(std::move(response), self.m_current_transaction_id);
+							}
+						);
+					},
+					self.m_currently_received_message
+				);
+
+				bytes_to_process = std::span{
+					std::begin(bytes_to_process) + bytes_consumed,
+					std::end(bytes_to_process)
+				};
+			}
+
+			return connection_status::ok;
+		}
+
+		[[nodiscard]] connection_status send_pending_messages()
+		{
+			auto flush = [this](){
+				return os_services::io::try_write(
+					m_registration.fd,
+					m_bytes_to_write,
+					[this](os_services::io::io_blocked){
+						enable_write_listening();
+					},
+					[this](os_services::io::fd_closed){
+						m_registration.event_handler_store->remove(m_registration.id);
+					}
+				);
+			};
+
+			if(!flush())
+			{ return connection_status::closed; }
+
+			std::span serialize_into{m_output_buffer.get(), m_buffer_size};
+			size_t bytes_ready = 0;
+			size_t bytes_left_to_use = m_buffer_size;
+			while(!m_msgs_to_send.empty() && !serialize_into.empty())
+			{
+				auto const bytes_written = std::visit(
+					[this, serialize_into](auto& item){
+						auto const ret = item.encode(serialize_into);
+						if(item.completed())
+						{
+							m_msgs_to_send.pop();
+							// WARNING: item is dead now
+						}
+						return ret;
+					},
+					m_msgs_to_send.front()
+				);
+
+				bytes_left_to_use -= bytes_written;
+				bytes_ready += bytes_written;
+				serialize_into = std::span{m_output_buffer.get() + bytes_written, bytes_left_to_use};
+			}
+
+			m_bytes_to_write = std::span{static_cast<std::byte const*>(m_output_buffer.get()), bytes_ready};
+			if(!flush())
+			{ return connection_status::closed; }
+
+			if(m_msgs_to_send.empty() && m_bytes_to_write.empty())
+			{ disable_write_listening(); }
+
+			return connection_status::ok;
+	}
+
+		template<class T>
+		void send(T&& msg, worker_sync::transaction_id tx_id)
+		{
+			auto const msg_id = utils::variant_index_v<T, outgoing_msg_type>;
+			m_msgs_to_send.push(
+				worker_sync::encoder<worker_sync::msg_header>{
+					worker_sync::msg_header{
+						.msg_id = msg_id,
+						.tx_id = tx_id
+					}
+				}
+			);
+			m_msgs_to_send.push(
+				worker_sync::encoder<std::remove_cvref_t<T>>{std::forward<T>(msg)}
+			);
+			std::ignore = send_pending_messages();
+		}
+
+	protected:
+		void handle_message(worker_sync::msg_header header)
+		{
+			m_current_transaction_id = header.tx_id;
+			if(header.msg_id == std::numeric_limits<decltype(header.msg_id)>::max())
+			{ throw std::runtime_error{"Invalid type-id"}; }
+			m_currently_received_message = utils::make_variant<msg_decoder>(header.msg_id + 1);
+		}
+
+	private:
+		size_t m_buffer_size;
+		// Decoder
+		std::unique_ptr<std::byte[]> m_input_buffer;
+		std::span<std::byte const> m_bytes_left_to_process;
+		msg_decoder m_currently_received_message;
+		transaction_id m_current_transaction_id;
+
+		// Encoder
+		std::unique_ptr<std::byte[]> m_output_buffer;
+		std::span<std::byte const> m_bytes_to_write;
+		std::queue<msg_encoder> m_msgs_to_send;
+		bool m_is_listening_for_write{false};
+
+		fd_activity_registred_event m_registration;
+
+		void enable_write_listening()
+		{
+			if(!m_is_listening_for_write)
+			{
+				m_registration.event_handler_store->update_listening_status(
+					m_registration.event_handler,
+					os_services::fd::activity_status::read_or_write
+				);
+				m_is_listening_for_write = true;
+			}
+		}
+
+		void  disable_write_listening()
+		{
+			if(m_is_listening_for_write)
+			{
+				m_registration.event_handler_store->update_listening_status(
+					m_registration.event_handler,
+					os_services::fd::activity_status::read
+				);
+				m_is_listening_for_write = false;
+			}
+		}
+	};
+}
+
+#endif
