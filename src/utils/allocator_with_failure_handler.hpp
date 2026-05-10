f15b087ac59b5bfe3a551df8908fd140346039d9
Torbjörn Rathsman <milasudril@gmail.com>
2026-05-10 17:30:55 +0200

    Make it possible to create allocator_with_failure_handler with non-default constructible objects

---
+++ src/utils/allocator_with_failure_handler.hpp
@@ -0,0 +1,61 @@
+#ifndef PIPE_UTILS_ALLOCATOR_WITH_FAILURE_HANDLER_HPP
+#define PIPE_UTILS_ALLOCATOR_WITH_FAILURE_HANDLER_HPP
+
+#include <new>
+#include <limits>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+namespace Pipe::utils
+{
+	template <class T, class FailureHandler>
+	class allocator_with_failure_handler
+	{
+	public:
+		using value_type = T;
+
+		allocator_with_failure_handler() = default;
+		explicit allocator_with_failure_handler(FailureHandler const& failure_handler):
+			m_failure_handler{failure_handler}
+		{}
+
+    template<class Other>
+    allocator_with_failure_handler(allocator_with_failure_handler<Other, FailureHandler> const& other):
+			m_failure_handler{other.failure_handler()}
+    {}
+
+		[[nodiscard]] T* allocate(std::size_t n)
+		{
+			auto const max_num_elements = std::numeric_limits<size_t>::max()/sizeof(T);
+			if(n > max_num_elements)
+			{ throw std::bad_array_new_length{}; }
+
+			auto const num_bytes_to_allocate = n*sizeof(T);
+			auto const ret = ::operator new(num_bytes_to_allocate, std::nothrow);
+			if(ret == nullptr) [[unlikely]]
+			{
+				m_failure_handler.memory_allocation_failed(std::type_identity<T>{}, n);
+				throw std::bad_alloc{};
+			}
+
+			return static_cast<T*>(ret);
+		}
+
+		static void deallocate(T* p, std::size_t n) noexcept
+		{ ::operator delete(p, n * sizeof(T)); }
+
+		bool operator==(allocator_with_failure_handler const&) const = default;
+		bool operator!=(allocator_with_failure_handler const&) const = default;
+
+		auto& failure_handler() noexcept
+		{ return m_failure_handler; }
+
+		auto& failure_handler() const noexcept
+		{ return m_failure_handler; }
+
+	private:
+		[[no_unique_address]] FailureHandler m_failure_handler;
+	};
+}
+#endif
---
+++ src/utils/allocator_with_failure_handler.test.cpp
@@ -0,0 +1,129 @@
+//@	{"target":{"name":"allocator_with_failure_handler.test"}}
+
+#include "./allocator_with_failure_handler.hpp"
+
+#include <testfwk/testfwk.hpp>
+#include <dlfcn.h>
+#include <deque>
+
+namespace
+{
+	struct allocation_failure_handler
+	{
+		void memory_allocation_failed(std::type_identity<int>, size_t n)
+		{
+			throw std::runtime_error{std::format("Failed to allocate {} ints", n)};
+		}
+	};
+
+	struct allocation_failure_handler_2
+	{
+		allocation_failure_handler_2(int){}
+
+		void memory_allocation_failed(std::type_identity<int>, size_t n)
+		{
+			throw std::runtime_error{std::format("Failed to allocate {} ints", n)};
+		}
+
+		void memory_allocation_failed(std::type_identity<int*>, size_t n)
+		{
+			throw std::runtime_error{std::format("Failed to allocate {} int*s", n)};
+		}
+	};
+	bool fail_next_malloc = false;
+}
+
+extern "C"
+{
+	void* malloc(size_t n)
+	{
+		if(fail_next_malloc)
+		{
+			fail_next_malloc = false;
+			return nullptr;
+		}
+
+		auto real_malloc = reinterpret_cast<void* (*)(size_t)>(dlsym(RTLD_NEXT, "malloc"));
+		return real_malloc(n);
+	}
+}
+
+TESTCASE(Pipe_utils_allocator_with_failure_handler_out_of_vm)
+{
+	try
+	{
+		std::vector<
+			int,
+			Pipe::utils::allocator_with_failure_handler<int, allocation_failure_handler>
+		>
+		buffer(4611686018427387905ll);
+		REQUIRE_EQ(false, true);
+	}
+	catch(std::length_error const&)
+	{}
+	catch(std::bad_array_new_length const&)
+	{}
+	catch(...)
+	{ REQUIRE_EQ(false, true); }
+}
+
+TESTCASE(Pipe_utils_allocator_with_failure_handler_malloc_succeeds)
+{
+	std::vector<
+		int,
+		Pipe::utils::allocator_with_failure_handler<int, allocation_failure_handler>
+	>
+	buffer;
+	buffer.push_back(235);
+	EXPECT_EQ(std::size(buffer), 1);
+}
+
+TESTCASE(Pipe_utils_allocator_with_failure_handler_malloc_fails)
+{
+	std::vector<
+		int,
+		Pipe::utils::allocator_with_failure_handler<int, allocation_failure_handler>
+	>
+	buffer;
+	fail_next_malloc = true;
+	try
+	{
+		buffer.push_back(235);
+		REQUIRE_EQ(false, true);
+	}
+	catch(std::exception const& err)
+	{
+		EXPECT_EQ(err.what(), std::string_view{"Failed to allocate 1 ints"});
+	}
+	EXPECT_EQ(std::size(buffer), 0);
+}
+
+TESTCASE(Pipe_utils_allocator_with_failure_handler_non_default_constructible_handler)
+{
+	using allocator =
+		Pipe::utils::allocator_with_failure_handler<
+			int,
+			allocation_failure_handler_2
+		>;
+
+	std::vector<
+		int,
+		Pipe::utils::allocator_with_failure_handler<int, allocation_failure_handler_2>
+	>
+	buffer(allocator{allocation_failure_handler_2{123}});
+}
+
+TESTCASE(Pipe_utils_allocator_with_failure_handler_non_default_constructible_handler_deque)
+{
+	using allocator =
+		Pipe::utils::allocator_with_failure_handler<
+			int,
+			allocation_failure_handler_2
+		>;
+
+	std::deque<
+		int,
+		allocator
+	>
+	buffer(allocator{allocation_failure_handler_2{123}});
+}
