e1ff50951411854dc36b8521932df78ad51ca1ac
Torbjörn Rathsman <milasudril@gmail.com>
2026-05-14 15:54:39 +0200

    Add alias for os_services::error_handling::code

--- src/os_services/error_handling/system_error.hpp
+++ src/os_services/error_handling/system_error.hpp
@@ -1,6 +1,8 @@
 #ifndef PIPE_OS_SERVICES_ERROR_HANDLING_SYSTEM_ERROR_HPP
 #define PIPE_OS_SERVICES_ERROR_HANDLING_SYSTEM_ERROR_HPP
 
+#include "src/common_fwk/error_handler.hpp"
+
 #include <cstring>
 #include <stdexcept>
 #include <format>
@@ -9,7 +11,7 @@
 
 namespace Pipe::os_services::error_handling
 {
-	enum class code{};
+	using code = common_fwk::os_error_code;
 
 	inline auto get_error_code() noexcept
 	{ return static_cast<code>(errno); }
