65d16ae05c639cde87ff6af39082b4c6d1a5496d
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-03 18:56:44 +0200

    Add output_port_activity_subscription class template

---
+++ src/worker_sync/output_port_activity_subscription.hpp
@@ -0,0 +1,29 @@
+#ifndef PIPE_WORKER_SYNC_OUTPUT_PORT_ACTIVITY_SUBSCRIPTION_HPP
+#define PIPE_WORKER_SYNC_OUTPUT_PORT_ACTIVITY_SUBSCRIPTION_HPP
+
+#include "./worker_sync.hpp"
+
+#include "src/utils/unwrap.hpp"
+
+namespace Pipe::worker_sync
+{
+	template<class OutputPortActivitySubscriber, class OutputPort>
+	class output_port_activity_subscription
+	{
+	public:
+		void notify_data_ready()
+		{ m_subscriber.notify_data_ready(m_id); }
+
+		void notify_client_ready()
+		{
+			utils::unwrap(m_output_port).notify_client_ready(this, m_id);
+		}
+
+	private:
+		worker_sync::port_activity_subscription_id m_id;
+		OutputPortActivitySubscriber m_subscriber;
+		OutputPort m_output_port;
+
+	};
+}
+#endif
---
+++ src/worker_sync/output_port_activity_subscription.test.cpp
@@ -0,0 +1,5 @@
+//@	{"target":{"name":"output_port_activity_subscription.test"}}
+
+#include "./output_port_activity_subscription.hpp"
+
+#include <testfwk/testfwk.hpp>
