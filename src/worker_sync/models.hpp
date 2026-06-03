2d2764f408f3cc5256c80d43ecd1c8680db780f6
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-03 20:52:14 +0200

    More work on models

--- src/worker_sync/models.hpp
+++ src/worker_sync/models.hpp
@@ -1,6 +1,9 @@
 #ifndef PIPE_WORKER_SYNC_MODELS_HPP
 #define PIPE_WORKER_SYNC_MODELS_HPP
 
+#include "./worker_sync_msg.hpp"
+#include "src/utils/unwrap.hpp"
+
 #include <concepts>
 
 namespace Pipe::worker_sync
@@ -9,6 +12,16 @@ namespace Pipe::worker_sync
 	concept output_port_activity_subscription_model = requires(T& obj)
 	{
 		{ obj.notify_data_ready() } -> std::same_as<void>;
+	};
+
+	template<class T, class U>
+	concept output_port_model =
+		   output_port_activity_subscription_model<decltype(utils::unwrap(std::declval<U>()))>
+		&& requires(T& obj,U subscriber)
+	{
+		{ obj.dec_num_ready_subscribers(subscriber) } -> std::same_as<void>;
+		{ obj.add_subscriber(subscriber) } -> std::same_as<void>;
+		{ obj.remove_subscriber(subscriber) } -> std::same_as<void>;
 	};
 }
 
