5fc3e3511fe4b045e3357362a7309226af9abfbf
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-05 21:14:58 +0200

    Add test case for output_port_activity_subscription

--- src/worker_sync/output_port_activity_subscription.hpp
+++ src/worker_sync/output_port_activity_subscription.hpp
@@ -2,12 +2,12 @@
 #define PIPE_WORKER_SYNC_OUTPUT_PORT_ACTIVITY_SUBSCRIPTION_HPP
 
 #include "./worker_sync_msg.hpp"
-
+#include "src/utils/unwrap.hpp"
 #include <utility>
 
 namespace Pipe::worker_sync
 {
-	template<class OutputPortActivitySubscriber, class OutputPort>
+	template<class OutputPort, class OutputPortActivitySubscriber>
 	class output_port_activity_subscription
 	{
 	public:
@@ -24,23 +24,9 @@ namespace Pipe::worker_sync
 		~output_port_activity_subscription() noexcept
 		{ reset(); }
 
-		output_port_activity_subscription(output_port_activity_subscription&& other) noexcept:
-			m_id{std::exchange(other.m_id, worker_sync::port_activity_subscription_id{})},
-			m_output_port{std::exchange(other.m_output_port, nullptr)},
-			m_subscriber{std::exchange(other.m_subscriber, OutputPortActivitySubscriber{})}
-		{ }
-
-		output_port_activity_subscription& operator=(output_port_activity_subscription&& other) noexcept
-		{
-			reset();
-			m_id = std::exchange(other.m_id, worker_sync::port_activity_subscription_id{});
-			m_subscriber = std::exchange(other.m_subscriber, OutputPortActivitySubscriber{});
-			m_output_port = std::exchange(other.m_output_port, nullptr);
-			return *this;
-		}
-
+		output_port_activity_subscription(output_port_activity_subscription&& other) = delete;
+		output_port_activity_subscription& operator=(output_port_activity_subscription&& other) = delete;
 		output_port_activity_subscription(output_port_activity_subscription const&) = delete;
-
 		output_port_activity_subscription& operator=(output_port_activity_subscription const&) = delete;
 
 		void reset() noexcept
@@ -50,7 +36,7 @@ namespace Pipe::worker_sync
 		}
 
 		void notify_data_ready()
-		{ m_subscriber.notify_data_ready(m_id); }
+		{ utils::unwrap(m_subscriber).notify_data_ready(m_id); }
 
 		void notify_client_ready()
 		{ m_output_port->dec_num_busy_subscribers(this); }
--- src/worker_sync/output_port_activity_subscription.test.cpp
+++ src/worker_sync/output_port_activity_subscription.test.cpp
@@ -3,3 +3,74 @@
 #include "./output_port_activity_subscription.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+namespace
+{
+	struct output_port_activity_subscriber
+	{
+		std::optional<Pipe::worker_sync::port_activity_subscription_id> expected_subscription_id;
+
+		void notify_data_ready(Pipe::worker_sync::port_activity_subscription_id id)
+		{
+			EXPECT_EQ(expected_subscription_id, id);
+			expected_subscription_id.reset();
+		}
+
+		~output_port_activity_subscriber()
+		{ EXPECT_EQ(expected_subscription_id.has_value(), false); }
+	};
+
+	struct output_port
+	{
+		using subscription_type =
+			Pipe::worker_sync::output_port_activity_subscription<
+				output_port,
+				std::reference_wrapper<output_port_activity_subscriber>
+			>;
+
+		std::optional<subscription_type*> added_subscription;
+
+		void add_subscription(subscription_type* subscription)
+		{ added_subscription = subscription; }
+
+		void remove_subscription(subscription_type* subscription)
+		{
+			EXPECT_EQ(added_subscription, subscription);
+			added_subscription.reset();
+		}
+
+
+		std::optional<subscription_type*> expected_subscription_decrement;
+		void dec_num_busy_subscribers(subscription_type* subscription)
+		{
+			EXPECT_EQ(expected_subscription_decrement, subscription);
+			expected_subscription_decrement.reset();
+		}
+
+		~output_port()
+		{
+			EXPECT_EQ(added_subscription.has_value(), false);
+			EXPECT_EQ(expected_subscription_decrement.has_value(), false);
+		}
+	};
+}
+
+TESTCASE(Pipe_worker_sync_ouptut_port_activity_subscription_do_stuff)
+{
+	Pipe::worker_sync::port_activity_subscription_id id{456};
+	output_port my_output_port;
+	output_port_activity_subscriber my_output_port_activity_subscriber;
+
+	Pipe::worker_sync::output_port_activity_subscription subscription{
+		id,
+		&my_output_port,
+		std::ref(my_output_port_activity_subscriber)
+	};
+	EXPECT_EQ(my_output_port.added_subscription, &subscription);
+
+	my_output_port_activity_subscriber.expected_subscription_id = id;
+	subscription.notify_data_ready();
+
+	my_output_port.expected_subscription_decrement = &subscription;
+	subscription.notify_client_ready();
+}
