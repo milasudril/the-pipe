c9ab16d4a1382d9b8508a5a2a0a782b06ca05841
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-13 21:15:57 +0200

    Improve sync_client_connection.test.cpp

--- src/worker_fwk/sync_client_connection.test.cpp
+++ src/worker_fwk/sync_client_connection.test.cpp
@@ -8,6 +8,8 @@
 #include "src/os_services/ipc/unix_domain_socket.hpp"
 #include "src/os_services/ipc/socket_pair.hpp"
 #include "src/utils/utils.hpp"
+#include "src/worker_fwk/port_activity_subscriber.hpp"
+#include "src/worker_sync/worker_sync_msg.hpp"
 #include "testfwk/testsuite.hpp"
 #include "testfwk/validation.hpp"
 
@@ -23,13 +25,10 @@ namespace
 {
 	struct my_port_activity_subscription_registry
 	{
+		std::optional<Pipe::worker_fwk::port_activity_subscriber_ref> saved_subscriber_ref;
+
 		std::optional<std::string> expected_server_portname;
-		bool fail_port_activity_subscription = false;
-
-		std::optional<Pipe::worker_sync::port_activity_subscription_id> remove_expected_subscription_id;
-		std::optional<Pipe::worker_sync::port_activity_subscription_id> notify_expected_subscription_id;
-		Pipe::worker_fwk::port_activity_subscriber_ref saved_subscriber_ref;
-
+		std::optional<Pipe::worker_sync::port_activity_subscription_id> returned_subscription_id;
 		Pipe::worker_sync::port_activity_subscription_id add_port_activity_subscription(
 			std::string const& server_portname,
 			Pipe::worker_fwk::port_activity_subscriber_ref subscriber_ref
@@ -37,34 +36,61 @@ namespace
 		{
 			EXPECT_EQ(server_portname, expected_server_portname);
 			expected_server_portname.reset();
-			if(fail_port_activity_subscription)
-			{
-				fail_port_activity_subscription = false;
-				throw std::runtime_error{"Failed to add port activity subscription"};
-			}
 			saved_subscriber_ref = subscriber_ref;
-			return Pipe::worker_sync::port_activity_subscription_id{54};
+			REQUIRE_EQ(returned_subscription_id.has_value(), true);
+			auto const ret = *returned_subscription_id;
+			returned_subscription_id.reset();
+			return ret;
 		}
+
+		std::optional<Pipe::worker_sync::port_activity_subscription_id> remove_subscription_subscription_id;
+		std::optional<Pipe::worker_fwk::port_activity_subscriber_ref>  remove_subscription_subscriber;
 
 		void remove_port_activity_subscription(
 			Pipe::worker_sync::port_activity_subscription_id subscription_id,
-			Pipe::worker_fwk::port_activity_subscriber_ref
+			Pipe::worker_fwk::port_activity_subscriber_ref subscriber
 		)
 		{
-			EXPECT_EQ(subscription_id, remove_expected_subscription_id);
-			remove_expected_subscription_id.reset();
+			EXPECT_EQ(remove_subscription_subscription_id.has_value(), true);
+			EXPECT_EQ(remove_subscription_subscriber.has_value(), true);
+			EXPECT_EQ(subscription_id, remove_subscription_subscription_id);
+			EXPECT_EQ(subscriber, remove_subscription_subscriber);
+			remove_subscription_subscription_id.reset();
+			remove_subscription_subscriber.reset();
 		}
 
+		std::optional<Pipe::worker_sync::port_activity_subscription_id> notify_subscription_subscription_id;
+		std::optional<Pipe::worker_fwk::port_activity_subscriber_ref>  notify_subscription_subscriber;
 		void notify_client_ready(
 			Pipe::worker_sync::port_activity_subscription_id subscription_id,
-			Pipe::worker_fwk::port_activity_subscriber_ref
+			Pipe::worker_fwk::port_activity_subscriber_ref subscriber
 		)
 		{
-			EXPECT_EQ(subscription_id, notify_expected_subscription_id);
-			notify_expected_subscription_id.reset();
+			EXPECT_EQ(notify_subscription_subscription_id.has_value(), true);
+			EXPECT_EQ(notify_subscription_subscriber.has_value(), true);
+			EXPECT_EQ(subscription_id, notify_subscription_subscription_id);
+			EXPECT_EQ(subscriber, notify_subscription_subscriber);
+			notify_subscription_subscription_id.reset();
+			notify_subscription_subscriber.reset();
 		}
 
-		void remove_port_activity_subscriber(Pipe::worker_fwk::port_activity_subscriber_ref){}
+		std::optional<Pipe::worker_fwk::port_activity_subscriber_ref> remove_subscriber_subscriber;
+		void remove_port_activity_subscriber(Pipe::worker_fwk::port_activity_subscriber_ref subscriber)
+		{
+			EXPECT_EQ(remove_subscriber_subscriber.has_value(), true);
+			EXPECT_EQ(subscriber, remove_subscriber_subscriber);
+			remove_subscriber_subscriber.reset();
+		}
+
+		~my_port_activity_subscription_registry()
+		{
+			EXPECT_EQ(expected_server_portname.has_value(), false);
+			EXPECT_EQ(remove_subscription_subscription_id.has_value(), false);
+			EXPECT_EQ(remove_subscription_subscriber.has_value(), false);
+			EXPECT_EQ(notify_subscription_subscription_id.has_value(), false);
+			EXPECT_EQ(notify_subscription_subscriber.has_value(), false);
+			EXPECT_EQ(remove_subscriber_subscriber.has_value(), false);
+		}
 	};
 
 	using sync_client_connection = Pipe::worker_fwk::sync_client_connection<my_port_activity_subscription_registry>;
@@ -144,36 +170,7 @@ namespace
 	}
 }
 
-TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_subscription_request_fail_to_add_subscriber)
-{
-	my_port_activity_subscription_registry registry;
-	Pipe::worker_fwk::sync_client_connection conn{
-		std::ref(registry),
-		65536
-	};
-
-	Pipe::worker_sync::exception_controller ec;
-
-	try
-	{
-		registry.expected_server_portname = "Foobar";
-		registry.fail_port_activity_subscription = true;
-		conn.handle_request(
-			Pipe::worker_sync::port_activity_subscription_request{
-				.server_portname = "Foobar"
-			},
-			Pipe::worker_sync::transaction_id{325},
-			ec
-		);
-		abort();
-	}
-	catch(...)
-	{}
-	EXPECT_EQ(conn.num_messages_to_send(), 0);
-	EXPECT_EQ(ec.exceptions_should_be_rethrown(), false);
-}
-
-TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_subscription_request_succeeds)
+TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_subscription_request)
 {
 	my_event_handler_registry eh_registry;
 	my_port_activity_subscription_registry registry;
@@ -185,6 +182,7 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_subscription_reque
 	Pipe::worker_sync::exception_controller ec;
 
 	registry.expected_server_portname = "Foobar";
+	registry.returned_subscription_id = Pipe::worker_sync::port_activity_subscription_id{54};
 	conn.handle_request(
 		Pipe::worker_sync::port_activity_subscription_request{
 			.server_portname = "Foobar"
@@ -233,9 +231,11 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_subscription_reque
 		sockets.socket_b()
 	);
 	EXPECT_EQ(body.id, Pipe::worker_sync::port_activity_subscription_id{54});
+
+	registry.remove_subscriber_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 }
 
-TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_unsubscription_succeeds)
+TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_unsubscription)
 {
 	my_event_handler_registry eh_registry;
 	my_port_activity_subscription_registry registry;
@@ -247,7 +247,8 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_unsubscription_suc
 	Pipe::worker_sync::exception_controller ec;
 
 	// Remove subscription
-	registry.remove_expected_subscription_id = Pipe::worker_sync::port_activity_subscription_id{64365};
+	registry.remove_subscription_subscription_id = Pipe::worker_sync::port_activity_subscription_id{64365};
+	registry.remove_subscription_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 	conn.handle_request(
 		Pipe::worker_sync::port_activity_unsubscription{
 			.id = Pipe::worker_sync::port_activity_subscription_id{64365}
@@ -297,6 +298,8 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_port_activity_unsubscription_suc
 		);
 		EXPECT_EQ(body.id, Pipe::worker_sync::port_activity_subscription_id{64365});
 	}
+
+	registry.remove_subscriber_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 }
 
 TESTCASE(Pipe_worker_fwk_sync_client_connection_client_ready)
@@ -308,13 +311,16 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_client_ready)
 		65536
 	};
 
-	registry.notify_expected_subscription_id = Pipe::worker_sync::port_activity_subscription_id{54};
+	registry.notify_subscription_subscription_id = Pipe::worker_sync::port_activity_subscription_id{54};
+	registry.notify_subscription_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 	conn.handle_message(
 		Pipe::worker_sync::client_ready_event{
 			.id = Pipe::worker_sync::port_activity_subscription_id{54}
 		}
 	);
 	EXPECT_EQ(conn.num_messages_to_send(), 0);
+
+	registry.remove_subscriber_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 }
 
 TESTCASE(Pipe_worker_fwk_sync_client_connection_notify_data_ready)
@@ -367,4 +373,36 @@ TESTCASE(Pipe_worker_fwk_sync_client_connection_notify_data_ready)
 		sockets.socket_b()
 	);
 	EXPECT_EQ(body.id, Pipe::worker_sync::port_activity_subscription_id{4365});
+
+	registry.remove_subscriber_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
+}
+
+TESTCASE(Pipe_worker_fwk_sync_client_connection_handle_error_response)
+{
+	my_event_handler_registry eh_registry;
+	my_port_activity_subscription_registry registry;
+	Pipe::worker_fwk::sync_client_connection conn{
+		std::ref(registry),
+		65536
+	};
+	Pipe::worker_sync::exception_controller ec;
+
+	try
+	{
+		conn.handle_response(
+			Pipe::worker_sync::error_response{
+				.message = "This is the error message"
+			},
+			Pipe::worker_sync::transaction_id{},
+			ec
+		);
+		EXPECT_EQ(false, true);
+	}
+	catch(std::exception const& err)
+	{
+		EXPECT_EQ(err.what(), std::string_view{"This is the error message"});
+	}
+
+	EXPECT_EQ(ec.exceptions_should_be_rethrown(), true);
+	registry.remove_subscriber_subscriber = Pipe::worker_fwk::port_activity_subscriber_ref{conn};
 }
