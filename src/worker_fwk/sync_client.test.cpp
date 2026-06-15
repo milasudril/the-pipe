6af7eec36a9d33000689687062a88acd3196d9a1
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-15 15:03:25 +0200

    Test handle error response

--- src/worker_fwk/sync_client.test.cpp
+++ src/worker_fwk/sync_client.test.cpp
@@ -9,11 +9,21 @@ namespace
 {
 	struct input_port_activity_subscriber
 	{
-		void sync_client_lost_connection_to_server(void const*)
-		{}
+		std::optional<void const*> expected_conn_lost_ptr;
+		void sync_client_lost_connection_to_server(void const* ptr)
+		{
+			EXPECT_EQ(expected_conn_lost_ptr.has_value(), true);
+			EXPECT_EQ(expected_conn_lost_ptr, ptr);
+			expected_conn_lost_ptr.reset();
+		}
 
-		void notify_data_ready(Pipe::worker_sync::port_activity_subscription_id)
-		{}
+		std::optional<Pipe::worker_sync::port_activity_subscription_id> expected_data_ready_id;
+		void notify_data_ready(Pipe::worker_sync::port_activity_subscription_id id)
+		{
+			EXPECT_EQ(expected_data_ready_id.has_value(), true);
+			EXPECT_EQ(expected_data_ready_id, id);
+			expected_data_ready_id.reset();
+		}
 
 		void subscription_completed(
 			Pipe::worker_sync::transaction_id,
@@ -23,6 +33,12 @@ namespace
 
 		void unsubscription_completed(Pipe::worker_sync::transaction_id)
 		{}
+
+		~input_port_activity_subscriber()
+		{
+			EXPECT_EQ(expected_conn_lost_ptr.has_value(), false);
+			EXPECT_EQ(expected_data_ready_id.has_value(), false);
+		}
 	};
 
 };
@@ -31,4 +47,29 @@ TESTCASE(Pipe_worker_fwk_sync_client_construct)
 {
 	input_port_activity_subscriber subscriber;
 	Pipe::worker_fwk::sync_client client{std::ref(subscriber)};
+	subscriber.expected_conn_lost_ptr = &client;
 }
+
+TESTCASE(Pipe_worker_fwk_sync_client_handle_error_response)
+{
+	input_port_activity_subscriber subscriber;
+	Pipe::worker_fwk::sync_client client{std::ref(subscriber)};
+	Pipe::worker_sync::exception_controller ec;
+	try
+	{
+		client.handle_response(
+			Pipe::worker_sync::error_response{
+				.message = "The error message"
+			},
+			Pipe::worker_sync::transaction_id{4324},
+			ec
+		);
+		EXPECT_EQ(false, true);
+	}
+	catch(std::exception const& err)
+	{
+		EXPECT_EQ(err.what(), std::string_view{"The error message"});
+	}
+	subscriber.expected_conn_lost_ptr = &client;
+}
+
